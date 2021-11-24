1ed65a0e1a19af2ce67246a79ccea28bd5d4b188
JyotirmayaMajhi <jyotirmayamajhi1998@gamil.com>
2021-11-24 22:49:29 +0530

    Modified: Password with atleast one numeric

--- src/com/bridgelabz/userregistration/Pattern.java
+++ src/com/bridgelabz/userregistration/Pattern.java
@@ -57,10 +57,10 @@ public class Pattern {
 	public void addPassword() {
 		System.out.print("Enter Password : ");
 		password = sc.nextLine();
-		boolean check = password.matches("^(?=.*[A-Z]).[a-zA-Z]{8,}$");
+		boolean check = password.matches("^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$");
 		if (check == true) {
 		} else {
-			System.out.println("Invalid password format \\nAdd again [Atleast 8 Character and 1 Upper Case ]");
+			System.out.println("Invalid password format \\nAdd again [Atleast 8 Character, 1 Upper Case, Atleast one numeric number]");
 			addPassword();
 		}
 	}
